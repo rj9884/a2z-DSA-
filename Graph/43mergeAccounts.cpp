8d36697a606d6dde47b40d3ad04bf2c81b1fd81b
rj9884 <rj.vidyagyan@gmail.com>
2026-07-20 20:18:40 +0530

    Graph/43 merge accounts

---
+++ Graph/43mergeAccounts.cpp
@@ -0,0 +1,147 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
+class DisjointSet {
+    vector<int> rank, parent, size;
+
+public:
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    int findUPar(int node) {
+        if (node == parent[node]) {
+            return node;
+        }
+
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+
+        if (ulp_u == ulp_v) {
+            return;
+        }
+
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        } else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        } else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+
+        if (ulp_u == ulp_v) {
+            return;
+        }
+
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        } else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
+class Solution {
+public:
+    vector<vector<string>> accountsMerge(vector<vector<string>> &details) {
+        int n = details.size();
+
+        DisjointSet ds(n);
+
+        unordered_map<string, int> mapMailNode;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < details[i].size(); j++) {
+                string mail = details[i][j];
+
+                if (mapMailNode.find(mail) == mapMailNode.end()) {
+                    mapMailNode[mail] = i;
+                } else {
+                    ds.unionBySize(i, mapMailNode[mail]);
+                }
+            }
+        }
+
+        vector<vector<string>> mergedMail(n);
+
+        for (auto &it : mapMailNode) {
+            string mail = it.first;
+            int node = ds.findUPar(it.second);
+            mergedMail[node].push_back(mail);
+        }
+
+        vector<vector<string>> ans;
+
+        for (int i = 0; i < n; i++) {
+            if (mergedMail[i].empty()) {
+                continue;
+            }
+
+            sort(mergedMail[i].begin(), mergedMail[i].end());
+
+            vector<string> temp;
+            temp.push_back(details[i][0]);
+
+            for (auto &mail : mergedMail[i]) {
+                temp.push_back(mail);
+            }
+
+            ans.push_back(temp);
+        }
+
+        sort(ans.begin(), ans.end());
+
+        return ans;
+    }
+};
+
+/*Time: O(N+E) + O(E*4ɑ) + O(N*(ElogE + E)) where N = no. of indices or nodes and E = no. of emails
+Space: O(N)+ O(N) +O(2N) ~ O(N) where N = no. of nodes/indices */
+
+int main() {
+    Solution obj;
+
+    vector<vector<string>> accounts = {
+        {"John", "j1@com", "j2@com", "j3@com"},
+        {"John", "j4@com"},
+        {"Raj", "r1@com", "r2@com"},
+        {"John", "j1@com", "j5@com"},
+        {"Raj", "r2@com", "r3@com"},
+        {"Mary", "m1@com"}
+    };
+
+    vector<vector<string>> ans = obj.accountsMerge(accounts);
+
+    for (auto &account : ans) {
+        for (auto &item : account) {
+            cout << item << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
